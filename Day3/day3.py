6d7c35aefaada87a5db26c27b9c9d065209fd12e
Stephen Chavez <stephen@dicesoft.net>
2018-12-12 16:55:17 -0700

    Day 3

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,4 +1,8 @@
 # import numpy
+### Day 3 Solution by Stephen Chavez
+### Date: Dec 11, 2018
+### Based on existing Solution : https://github.com/Kurocon/AdventOfCode2018/blob/master/days/day3.py
+
 import re
 from collections import defaultdict
 
@@ -20,9 +24,19 @@ def main():
 
 
     overlapping_squares = sum([1 for key, value in squares.items() if len(value) > 1])
+    print("There are {} overlapping squares".format(overlapping_squares))
 
     unique_claims = set(x['claim_id'] for x in parsed_input)
-    print("There are {} overlapping squares".format(overlapping_squares))
+    overlapping_claims = set()
+
+    # PART 2
+    for key, value in squares.items():
+        if len(value) > 1:
+            for v in value:
+                overlapping_claims.add(v)
+
+    result = unique_claims - overlapping_claims
+    print ("The unique claim is {}".format(result))
 
 def readFile():
     PARSE_REGEX = r'#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)'
